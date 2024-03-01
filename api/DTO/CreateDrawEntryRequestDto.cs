9e81b57eea2f38a8614e3323d4c5533bf1cb597b
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-01 14:06:27 +0100

    add dto for drawentry

---
+++ api/DTO/CreateDrawEntryRequestDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.DTO;
+
+public class CreateDrawEntryRequestDto
+{
+    [Required] [MaxLength(50)] public string FirstName { get; set; }
+
+    [Required] [MaxLength(50)] public string LastName { get; set; }
+
+    [Required] [EmailAddress] public string EmailAddress { get; set; }
+
+    [Required] public string SerialNumber { get; set; }
+}
