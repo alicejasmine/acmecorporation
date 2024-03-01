339393777ee12caf4b5da8db7faec1cfdbd92061
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-01 14:05:56 +0100

    add controller with post end point

---
+++ api/Controllers/DrawEntryController.cs
@@ -0,0 +1,36 @@
+using api.DataValidation;
+using api.DTO;
+using infrastructure.DataModel;
+using Microsoft.AspNetCore.Mvc;
+using service;
+
+namespace api.Controllers;
+
+
+[ApiController]
+
+public class DrawEntryController : ControllerBase
+{
+    private readonly DrawEntryService _drawEntryService;
+
+    public DrawEntryController(DrawEntryService drawEntryService)
+    {
+        _drawEntryService = drawEntryService;
+    }
+
+    // post draw entry
+
+    [HttpPost]
+    [ValidateModel]
+    [Route("/api/entry")]
+    public DrawEntry Post([FromBody] CreateDrawEntryRequestDto dto)
+    {
+        HttpContext.Response.StatusCode = StatusCodes.Status201Created;
+        return _drawEntryService.CreateEntry();
+    }
+
+
+}
+
+
+
