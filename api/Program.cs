ddced0326740eaca6da8cd092dedd291c715f694
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-01 14:07:11 +0100

    add singleton for dependency injection

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,14 @@
+
+using infrastructure;
+using service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<DrawEntryRepository>();
+builder.Services.AddSingleton<DrawEntryService>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
