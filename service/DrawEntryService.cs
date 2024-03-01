226249e3bd0683fbb637235d4aefc95bf4b4bfc3
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-01 14:07:33 +0100

    add service class for drawentry

---
+++ service/DrawEntryService.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using infrastructure;
+using infrastructure.DataModel;
+
+namespace service;
+
+public class DrawEntryService
+{
+    private readonly DrawEntryRepository _drawEntryRepository;
+
+    public DrawEntryService(DrawEntryRepository drawEntryRepository)
+    {
+        _drawEntryRepository = drawEntryRepository;
+    }
+
+
+    public DrawEntry CreateEntry()
+    {
+        try
+        {
+            //if (_drawEntryRepository.IsFullNameTakenInCreate(fullname))
+              //  throw new ValidationException("player name is taken");
+            return _drawEntryRepository.CreateEntry();
+        }
+        catch (ValidationException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.InnerException?.Message);
+            throw;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.InnerException?.Message);
+            throw new Exception("Could not create entry");
+        }
+    }
+}
