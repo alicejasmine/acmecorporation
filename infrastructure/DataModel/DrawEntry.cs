9c61c1782fff1f11cca6bd01b0747ce713f61b97
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-01 14:04:14 +0100

    add model for draw entry

---
+++ infrastructure/DataModel/DrawEntry.cs
@@ -0,0 +1,12 @@
+namespace infrastructure.DataModel;
+
+public class DrawEntry
+{
+  
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string SerialNumber { get; set; }
+    
+}
