aebebbd1b1be53e1053f396f07c58386296ceeee
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-02 15:50:39 +0100

    database connection settings and create entry in db

--- infrastructure/DrawEntryRepository.cs
+++ infrastructure/DrawEntryRepository.cs
@@ -1,11 +1,42 @@
-﻿using infrastructure.DataModel;
+﻿using Dapper;
+using infrastructure.DataModel;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
 
 namespace infrastructure;
 
 public class DrawEntryRepository
 {
-    public DrawEntry CreateEntry()
+    private readonly string _connectionString;
+
+
+    public DrawEntryRepository(IConfiguration config)
     {
-        throw new NotImplementedException();
+        _connectionString = Utilities.GetConnectionString();
+    }
+
+    public DrawEntry CreateEntry(string firstName, string lastName, string emailAddress, string serialNumber)
+    {
+        var sql = $@"
+                INSERT INTO dbo.DrawEntries (first_name, last_name, email_address, serial_number)
+                OUTPUT INSERTED.*
+                VALUES (@firstName, @lastName, @emailAddress, @serialNumber);
+        
+                 ;";
+
+        using (var conn = new SqlConnection(_connectionString))
+
+        {
+            Console.WriteLine("here: " + _connectionString);
+
+            return conn.QueryFirst<DrawEntry>(sql, new
+            {
+                firstName,
+                lastName,
+                emailAddress,
+                serialNumber
+            });
+        }
     }
 }
