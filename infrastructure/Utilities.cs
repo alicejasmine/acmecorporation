8f68532a823b69f34896ace4e5c0c8cf53c67c1a
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-02 15:52:00 +0100

    dd class to retrieve connection string from IDE

---
+++ infrastructure/Utilities.cs
@@ -0,0 +1,21 @@
+﻿namespace infrastructure;
+
+public class Utilities
+{
+    private const string DefaultServer = "localhost";
+    private const int DefaultPort = 1433;
+    private static readonly string EnvironmentVariableName = "sqlconn";
+
+    public static string BuildConnectionString(
+        string server = DefaultServer,
+        string database = "",
+        string user = "",
+        string password = "",
+        bool trustServerCertificate = true) =>
+        $"Server={server},{DefaultPort};Database={database};User Id={user};Password={password};TrustServerCertificate={trustServerCertificate.ToString().ToUpper()}";
+    public static string GetConnectionString()
+    {
+        string connectionString = Environment.GetEnvironmentVariable(EnvironmentVariableName);
+        return connectionString ?? BuildConnectionString();
+    }
+}
