b6934d19f5b21787bc95f08ab6d9fee7aa8fba75
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-05 14:46:54 +0100

    unit test Create successful

---
+++ tests/CreateEntryUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Dapper;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using NUnit.Framework;
+using Newtonsoft.Json;
+
+namespace tests;
+
+public class CreateEntryUnitTests
+{
+    [TestCase("Andrea", "Clay", "andreac@gmail.com", "XHMXCLGXBFSUU959WLLW", true)]
+    public async Task EntryCanSuccessfullyBeCreatedFromHttpRequest(string fullname, string lastname,
+        string emailAddress, string serialNumber, bool isAgeConfirmed)
+    {
+        //ARRANGE
+        Helper.TriggerRebuild();
+
+        var testEntry = new CreateDrawEntryRequestDto
+        {
+            FirstName = fullname, LastName = lastname, EmailAddress = emailAddress, SerialNumber = serialNumber,
+            IsOver18Confirmed = isAgeConfirmed
+        };
+
+        //ACT
+        var httpResponse = await new HttpClient().PostAsJsonAsync(Helper.ApiBaseUrl + "/entry", testEntry);
+        DrawEntry entryFromResponseBody =
+            JsonConvert.DeserializeObject<DrawEntry>(await httpResponse.Content.ReadAsStringAsync());
+
+        //ASSERT
+        await using (var conn = new SqlConnection(Helper.GetConnectionString()))
+
+        {
+            var resultEntry = conn.QueryFirst<DrawEntry>(
+                "SELECT * FROM dbo.DrawEntries;");
+            resultEntry.Should()
+                .BeEquivalentTo(entryFromResponseBody);
+        }
+    }
+    
+    
+  
+}
