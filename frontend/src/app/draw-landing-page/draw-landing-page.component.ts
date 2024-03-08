bbbf67498468334bf9de9d12b1cf1b1a459c805e
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-08 17:45:13 +0100

    add formgroup for data validation in frontend

--- frontend/src/app/draw-landing-page/draw-landing-page.component.ts
+++ frontend/src/app/draw-landing-page/draw-landing-page.component.ts
@@ -1,13 +1,21 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-draw-landing-page',
-  template: `
-  `
+  template:  `
+   `
 
 })
 export class DrawLandingPageComponent {
-  
+  formGroup=new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    lastName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    emailAddress: new FormControl('', [Validators.required, Validators.email]),
+    serialNumber: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+    isOver18Confirmed: new FormControl(false, Validators.requiredTrue),
+    });
+
+
 
 }
