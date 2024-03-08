7c3f5fa015bc39900cb9b44ec067d93dfa9cf66a
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-08 17:43:36 +0100

    update routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DrawLandingPageComponent } from './draw-landing-page/draw-landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: DrawLandingPageComponent } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrawLandingPageComponent } from './draw-landing-page/draw-landing-page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawLandingPageComponent
   ],
   imports: [
     BrowserModule,
