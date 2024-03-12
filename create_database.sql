6f89d522d9cf7b5d41626ba563fbe6cee46833c2
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-12 19:30:58 +0100

    add file for db creation

---
+++ create_database.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE AcmeDB;
+
+USE AcmeDB
+CREATE TABLE DrawEntries
+(
+    entry_ID      INT PRIMARY KEY IDENTITY (1,1),
+    first_name    NVARCHAR(50)  NOT NULL,
+    last_name     NVARCHAR(50)  NOT NULL,
+    email_address NVARCHAR(255) NOT NULL,
+    serial_number NVARCHAR(20)  NOT NULL
+);
+
+USE AcmeDB
+CREATE TABLE ProductSerialNumbers
+(
+    Id            INT IDENTITY (1,1) PRIMARY KEY,
+    serial_number NVARCHAR(20) NOT NULL UNIQUE
+);
